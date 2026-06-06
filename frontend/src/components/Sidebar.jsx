860f54e377e871b90f15334a60b150b31216b0b5
Guciolek <124672898+Guciolek@users.noreply.github.com>
2026-06-06 15:21:44 +0200

    feat(sidebar): wire SessionActivityIndicator into session list

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AppLogoIcon, ChatIcon, LockIcon, StarIcon } from "./Icons";
+import SessionActivityIndicator from "./SessionActivityIndicator";
 
 const LANGUAGES = [
   {code:"en",label:"English"},{code:"hi",label:"हिन्दी"},{code:"ta",label:"தமிழ்"},
